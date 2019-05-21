700452f86c5e2f42c78dc92a6c288436fc6dcf0d
Harshul Jain <harshulrobo@gmail.com>
2019-05-21 16:05:40 +0530

    fix: json structure as per new version

--- QueryBuilder/QueryBuilder.py
+++ QueryBuilder/QueryBuilder.py
@@ -16,39 +16,38 @@ class QueryBuilder(object):
         columns = ','.join(query_dict['columns'])
         aggreation_col = query_dict['aggregation_columns']
         aggregation_type = query_dict['aggregation_type']
-        filter_query = self.construct_filter_query(query_dict['filters'])
+        filter_query = self.construct_filter_query(query_dict['query_filters'])
         query_str = "select {0}({1}) from {2} where {3}".format(aggregation_type, columns, table_name, filter_query)
         return query_str
     
-    def construct_filter_query(self, filters):
+    def construct_filter_query(self, filters_dict):
         # recrusive algorithm to construct the filter query
         query_ = ''
+        
+        group_relation = filters_dict['relation']
+        filters = filters_dict['filters']
         
         # iterate through every filter
         for i,filter_ in enumerate(filters):
             # make the query
-            child_filters = filter_['child_filters']
-            child_relation = filter_['child_relation']
-            var = filter_['var']
-            val = filter_['val']
-            op = filter_['op']
-            parent_relation = filter_['parent_relation']
-            flt_query = "{}{}{}".format(var, op, val)
-
-            if len(child_filters) ==0:
+            filter_type = filter_['filter_type']
+            
+            if filter_type == 'condition':
+                operand = filter_['operand']
+                operator = filter_['operator']
+                value = filter_['value']
+                filter_query = "{} {} {}".format(operand, operator, value)
                 if i==0:
-                    query_ += flt_query
+                    query_ += filter_query
                 else:
-                    query_ = "({} {} {})".format(query_, parent_relation, flt_query)
-
-
+                    query_ = "({} {} {})".format(query_, group_relation, filter_query)
             else:
-                child_query = self.construct_filter_query(child_filters)
-                flt_query = "({} {} {})".format(flt_query, child_relation, child_query)
+                nest_query = self.construct_filter_query(filter_)
                 if i==0:
-                    query_+=flt_query
+                    query_ += "{}".format(nest_query)
                 else:
-                    query_ = "({} {} {})".format(query_, parent_relation, flt_query)
+                    query_ = "({} {} {})".format(query_, group_relation, nest_query)
+                
         return query_
     
     
