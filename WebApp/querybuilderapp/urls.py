75f77680977c71a666d73ec8c826e8a64fae895f
Harshul Jain <harshulrobo@gmail.com>
2019-05-29 17:24:59 +0530

    implementation: plotting engine

--- WebApp/querybuilderapp/urls.py
+++ WebApp/querybuilderapp/urls.py
@@ -5,5 +5,5 @@ from querybuilderapp import views
 urlpatterns = [
     path('', views.home, name='qbhome',),
     url('^getschemadetails$', views.get_schema_details, name='getschemadetails'),
-
+    url('^get_query_data$', views.get_query_data, name='get_query_data'),
 ]
--- WebApp/querybuilderapp/views.py
+++ WebApp/querybuilderapp/views.py
@@ -6,6 +6,9 @@ from django.apps import apps
 import json
 import sqlite3
 import re
+import pandas as pd
+from django.db import connection
+
 
 myapp = apps.get_app_config('querybuilderapp')
 models_dict = myapp.models
@@ -25,3 +28,26 @@ def get_schema_details(request):
     model_class = models_dict[model_str]
     model_attr = get_model_fields(model_class)
     return JsonResponse(model_attr, safe=False)
+
+def get_query_data(request):
+    sql_query = request.POST.get('sql_query', None)
+    agg_type = request.POST.get('agg_type', None)
+    model_str = request.POST.get('table_name', None)
+
+    model_class = models_dict[model_str]
+    model_attr = get_model_fields(model_class)
+    print(model_attr)
+    parse_dates = {}
+    for column, column_type in model_attr:
+        if column_type == 'DateTimeField':
+            parse_dates[column] = {'format': '%d/%m/%y %H:%M'}
+
+    print(parse_dates)
+
+    df = pd.read_sql_query(sql_query, connection, parse_dates=['RecordDateTime'])
+
+    print(df.dtypes)
+
+    return JsonResponse(model_attr, safe=False)
+
+
